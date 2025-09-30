495f38f510714b784028e5215404d2ff903ef14f
hsleeReco <hslee@recokr.com>
2025-09-30 23:28:00 +0900

    이벤트 추가 중...

--- src/data/schemas/event.ts
+++ src/data/schemas/event.ts
@@ -1,6 +1,8 @@
 export enum EVENT_TYPE {
   EVENT = '이벤트',
   CASH_SHOP = '캐시샵',
+  ITEM = '아이템',
+  MAINTENANCE = '점검',
 }
 
 export interface GameEvent {
@@ -10,4 +12,6 @@ export interface GameEvent {
   startDate: Date
   endDate: Date
   description?: string
+  isRecurringWeekly?: boolean // 주간 반복 (매주 목요일)
+  isRecurringMonthly?: boolean // 월간 반복 (매월 마지막 날)
 }
